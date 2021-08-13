99890ef90540bd9f55a7164f2dea8c6ac178c57b
wolf-null <gelzestern@yandex.ru>
2021-08-13 13:49:09 +0300

    animation added
    Press space to stop

--- TheGame.py
+++ TheGame.py
@@ -2,9 +2,12 @@
 # TODO: Dependencies resources(numer of links)
 
 import random as rnd
+
+import keyboard
 import networkx
 from GraphGenerator import SwitchInitializer, GraphInitializer, BinomialInitializer
 import matplotlib.pyplot as plt
+import keyboard
 
 import numpy as np
 
@@ -68,12 +71,14 @@ class TheGame:
                 self.resource[transaction[0]] -= 1
                 self.resource[transaction[1]] += 1
 
-    def async_random_donation_link_based(self, sqrt_number_of_steps, max_donation):
+    def async_random_donation_link_based(self, sqrt_number_of_steps, max_donation, random_direction=True):
         for step in range(sqrt_number_of_steps):
-            print(step, 'of', sqrt_number_of_steps)
+            # print(step, 'of', sqrt_number_of_steps)
             for step_2 in range(sqrt_number_of_steps):
                 node_a = rnd.randint(0, self.graph.size-1)
                 node_b = list(self.graph.graph[node_a])[rnd.randint(0, len(self.graph.graph[node_a])-1)]
+                if rnd.random() >= 0.5 and random_direction:
+                    node_a, node_b = node_b, node_a
                 amount = rnd.randint(0, min(max_donation, self.resource[node_a]))
                 self.resource[node_a] -= amount
                 self.resource[node_b] += amount
@@ -88,14 +93,17 @@ class TheGame:
                 exp.add_edges_from([(k,j),(j,k)])
         return exp
 
-    def print_networkx(self):
+    def print_networkx(self, absolute_painting=True):
         # Find a max resource value:
         max_resource = max(self.resource)
         min_resource = min(self.resource)
 
         colors = list()
         for k in range(len(self.resource)):
-            colors += [((self.resource[k]-min_resource)/(max_resource-min_resource), 0, 0), ]
+            if absolute_painting:
+                colors += [(self.resource[k] / max_resource, 0, 0), ]
+            else:
+                colors += [((self.resource[k] - min_resource) / (max_resource - min_resource), 0, 0), ]
 
         """Visualization part"""
         figures, axes = plt.subplots(1,3, figsize=plt.figaspect(0.5))
@@ -108,13 +116,48 @@ class TheGame:
         axes[2].set_xlabel('Link count')
         axes[2].set_ylabel('Resource amount')
 
+        exp = self.export_networkx()
+
+        networkx.draw_networkx(self.export_networkx(), ax=axes[0], with_labels=False, node_size=12, edge_color='gray', node_color = colors)
+
+        figures.canvas.manager.window.attributes('-topmost',1)
+        plt.ion()
+        plt.show()
+        plt.suptitle('Hold [space] to end')
+        plt.pause(2.01)
+        return axes
+
+    def update_colors_networkx(self, the_axes, absolute_painting = True):
+        # Find a max resource value:
+        max_resource = max(self.resource)
+        min_resource = min(self.resource)
+
+        colors = list()
+        for k in range(len(self.resource)):
+            if absolute_painting:
+                colors += [(self.resource[k] / max_resource, 0, 0), ]
+            else:
+                colors += [((self.resource[k] - min_resource) / (max_resource - min_resource), 0, 0), ]
+
+        """Visualization part"""
+        the_axes[1].clear()
+        the_axes[2].clear()
+        the_axes[1].hist(self.resource, density=True)
+
+        links_counts = [len(node) for node in self.graph.graph]
+        the_axes[2].scatter(links_counts, self.resource)
+        the_axes[2].set_xlabel('Link count')
+        the_axes[2].set_ylabel('Resource amount')
 
 
         exp = self.export_networkx()
 
-        networkx.draw_circular(self.export_networkx(), ax=axes[0], with_labels=False, node_size=12, edge_color='gray', node_color = colors)
-
-        plt.show()
+        # networkx.draw_networkx(self.export_networkx(), ax=axes[0], with_labels=False, node_size=12, edge_color='gray', node_color = colors)
+        # for k in range(self.graph.size):
+        the_axes[0].collections[0].set_facecolor(colors)
+        plt.draw()
+        # plt.pause(15)
+            #the_axes.collections
 
     def hist_resources(self):
         plt.figure()
@@ -123,42 +166,24 @@ class TheGame:
 
 
 
-
-from GraphGenerator import BinomialInitializer
-# sw = BinomialInitializer(0.04081632653061224)
 sw = SwitchInitializer(2)
 sw.build(20)
 sw.random_reconnect(10000)
-sw.burnout(15)
+sw.burnout(20)
 sw.random_reconnect(10000)
 sw.print_networkx()
 
-game = TheGame(sw, start_resource=100)
-game.async_random_donation_link_based(1000, 200)
-game.print_networkx()
-game.async_random_donation_link_based(1000, 200)
-game.print_networkx()
-game.async_random_donation_link_based(4000, 200)
-game.print_networkx()
+game = TheGame(sw, start_resource=1000)
+axs = game.print_networkx()
+
+while True:
+    game.async_random_donation_link_based(150, 10, random_direction=False)  #5, 10
+    game.update_colors_networkx(axs)
+    plt.pause(0.1)
+    if keyboard.is_pressed(' '):
+        plt.ioff()
+        plt.show()
+        break
 
 exit(0)
 
-
-"""
-sw = SwitchInitializer(3)
-sw.build(300)
-sw.random_reconnect(20000)
-sw.print_networkx()
-sw.burnout(500)
-sw.print_networkx()
-sw.random_reconnect(20000)
-sw.print_networkx()
-
-game = TheGame(sw, 10)
-game.hist_resources()
-game.print_networkx()
-game.sync_random_donation_link_based(150000, 0.5)
-print('histing...')
-game.hist_resources()
-exit(0)
-"""
